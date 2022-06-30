be68ee401bd4ec14ebae8037a40ed83be10c1d16
pennyflipper <saan.sinh.saelee@gmail.com>
2022-06-29 23:33:22 -0700

    added authentication

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 //jshint esversion:6
 
 // Very important that 'dotenv' is at the very top or you will not be able able to access the environment variable
-// You do not need a const and all you have to do is call 'config'
+// You do not need a const and all you have to do is call on 'config'
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "Apple Pie",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connects to default mongoDB port // useNewUrlParser will remove warning in the console
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+// If you are receiving deprecation warnings, use code below.
+// mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 // Now you can create new users adding it to the userDB
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -38,19 +63,68 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    // this will capture whatever the user types in the username field
-    email: req.body.username,
-    // this will capture whatever the user types in the password field
-    password: md5(eq.body.password),
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      // this will capture whatever the user types in the username field
+      email: req.body.username,
+      // this will capture whatever the user types in the password field
+      password: hash,
+    });
+
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
 
-  newUser.save(function (err) {
+  req.login(user, function (err) {
     if (err) {
       console.log(err);
     } else {
-      res.render("secrets");
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
+      });
     }
   });
 });
@@ -65,9 +139,11 @@ app.post("/login", function (req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
