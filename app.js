ee064811ab506e588099688f56e1459d944bd749
pennyflipper <saan.sinh.saelee@gmail.com>
2022-06-30 19:31:49 -0700

    added google oauth

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const { profile } = require("console");
 
-const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -33,27 +34,65 @@ app.use(passport.session());
 // Connects to default mongoDB port // useNewUrlParser will remove warning in the console
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-// If you are receiving deprecation warnings, use code below.
-// mongoose.set("useCreateIndex", true);
-
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // Now you can create new users adding it to the userDB
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+    }
+  )
+);
+
+// GET REQUEST
 
 app.get("/", function (req, res) {
   res.render("home");
 });
+
+app.get("/auth/google", function (req, res) {
+  console.log(profile);
+  passport.authenticate("google", { scope: ["profile"] });
+});
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/login", function (req, res) {
   res.render("login");
@@ -76,6 +115,8 @@ app.get("/logout", function (req, res) {
   res.redirect("/");
 });
 
+// POST REQUEST
+
 app.post("/register", function (req, res) {
   User.register(
     { username: req.body.username },
@@ -93,25 +134,6 @@ app.post("/register", function (req, res) {
   );
 });
 
-app.post("/register", function (req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    const newUser = new User({
-      // this will capture whatever the user types in the username field
-      email: req.body.username,
-      // this will capture whatever the user types in the password field
-      password: hash,
-    });
-
-    newUser.save(function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-});
-
 app.post("/login", function (req, res) {
   const user = new User({
     username: req.body.username,
@@ -125,26 +147,6 @@ app.post("/login", function (req, res) {
       passport.authenticate("local")(req, res, function () {
         res.redirect("/secrets");
       });
-    }
-  });
-});
-
-// This will check to see if there is a user that matches after they put their credentials in
-app.post("/login", function (req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
-        });
-      }
     }
   });
 });
